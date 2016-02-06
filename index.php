6732328ae89a99f3de5cd594d4939bf87a6614b9
Tom <tgoralczyk1@gmail.com>
2016-02-06 10:33:51 -0500

    Created classes for about, homepage, and contact

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 echo '<a href="index.php?page=about">About</a><br />';
-echo '<a href="index.php?page=contant">Contact</a><br />';
+echo '<a href="index.php?page=contact">Contact</a><br />';
 echo '<a href="index.php">Home</a><br />';
 
 echo '<h4>print_r($_GET)</h4>';
@@ -26,4 +26,27 @@ else
   $obj->post();
 }
 
+class about 
+{
+  public function get()
+  {
+    echo '<br />this is the "about" page';
+  }
+}
+
+class homepage
+{
+  public function get()
+  {
+    echo '<br />this is the "homepage"';
+  }
+}
+
+class contact
+{
+  public function get()
+  {
+    echo '<br />this is the "contact" page';
+  }
+}
 ?>
