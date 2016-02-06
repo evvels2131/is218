611e1a92216de5339aa501d54bfbd9ed6dd936b3
Tom <tgoralczyk1@gmail.com>
2016-02-06 10:25:08 -0500

    Made use of the super globals to do some checking and create new objects

--- index.php
+++ index.php
@@ -5,9 +5,25 @@ echo '<a href="index.php?page=contant">Contact</a><br />';
 echo '<a href="index.php">Home</a><br />';
 
 echo '<h4>print_r($_GET)</h4>';
-echo '<pre>' .print_r($_GET). '</pre>';
+print_r($_GET);
 
 echo '<h4>$_SERVER[\'REQUEST_METHOD\']</h4>';
 echo $_SERVER['REQUEST_METHOD'];
+
+if (isset($_GET['page'])) 
+{
+  $page = $_GET['page'];
+}
+//echo $page;
+$obj = new $page;
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET')
+{
+  $obj->get();
+}
+else
+{
+  $obj->post();
+}
 
 ?>
