b9d405b10a7b6c3752ff5e07f32d79f63ae7c184
Tom <tgoralczyk1@gmail.com>
2016-02-06 10:08:56 -0500

    Added a line of code that prints some information

--- index.php
+++ index.php
@@ -4,4 +4,6 @@ echo '<a href="index.php?page=about">About</a><br />';
 echo '<a href="index.php?page=contant">Contact</a><br />';
 echo '<a href="index.php">Home</a><br />';
 
+print_r($_GET);
+
 ?>
