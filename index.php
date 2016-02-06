40f25e263ba571be266adc0fd7e57f6001fd0b96
Tom <tgoralczyk1@gmail.com>
2016-02-06 10:20:29 -0500

    Added a line of code that displays the request method

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ echo '<a href="index.php?page=about">About</a><br />';
 echo '<a href="index.php?page=contant">Contact</a><br />';
 echo '<a href="index.php">Home</a><br />';
 
-print_r($_GET);
+echo '<h4>print_r($_GET)</h4>';
+echo '<pre>' .print_r($_GET). '</pre>';
+
+echo '<h4>$_SERVER[\'REQUEST_METHOD\']</h4>';
+echo $_SERVER['REQUEST_METHOD'];
 
 ?>
