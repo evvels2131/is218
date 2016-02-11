bdf97e043f35729d6ba74c5ab74e2e0ae187fb54
Tom <tgoralczyk1@gmail.com>
2016-02-11 17:44:09 -0500

    Created a function that displays a table when an array of objects is passed

--- index2.php
+++ index2.php
@@ -90,8 +90,6 @@ class Page
 	}
 	
 	// Create form from an array of input fields
-
-	
 	public function createForm($inputFields, $action)
 	{
 		?>
@@ -151,40 +149,6 @@ class Page
 	}
 }
 
-$inputFields = array(
-	"First name" => array(
-		"type" => "text",
-		"name" => "firstName",
-		"value" => "First Name"
-	),
-	"Last name" => array(
-		"type" => "text",
-		"name" => "lastName",
-		"value" => "Last Name"
-	),
-	"Middle name" => array(
-		"type" => "text",
-		"name" => "middleName",
-		"value" => "Middle Name"
-	),
-	"User password" => array(
-		"type" => "password",
-		"name" => "psw",
-		"value" => "Password"
-	),
-	"Male" => array(
-		"type" => "radio",
-		"name" => "gender",
-		"value" => "male",
-		"checked" => "checked"
-	),
-	"Female" => array(
-		"type" => "radio",
-		"name=" => "gender",
-		"value" => "female"
-	)
-);
-
 $inputFields2 = array(
 	'0' => array(
 		'desc' => 'First name:',
@@ -237,10 +201,6 @@ $inputFields2 = array(
 	),
 );
 
-echo '<pre>';
-print_r($inputFields2);
-echo '</pre>';
-
 // Testing
 $obj = new Page("Homepage");
 $obj->createHeader();
@@ -256,6 +216,148 @@ $obj->createForm($inputFields2, 'index2.php');
 
 $obj->setFooter("This is a sample footer.");
 $obj->createFooter();
+
+
+class Person
+{
+	private $_title;
+	private $_firstName;
+	private $_lastName;
+	private $_emailAddress;
+	
+	public function __construct($title, $fname, $lname, $email)
+	{
+		$this->_title = $title;
+		$this->_firstName = $fname;
+		$this->_lastName = $lname;
+		$this->_emailAddress = $email;
+	}
+	
+	public function test()
+	{
+		return get_object_vars($this);
+		//var_dump(get_object_vars($this));
+	}
+	// Getters and Setters would go here
+	public function getTitle()
+	{
+		return $this->_title;
+	}
+	
+	public function getFirstName()
+	{
+		return $this->_firstName;
+	}
+	
+	public function getLastName()
+	{
+		return $this->_lastName;
+	}
+	
+	public function getEmailAddress()
+	{
+		return $this->_emailAddress;
+	}
+}
+
+$john = new Person('Waiter', 'John', 'Smith', 'johnsmith@njit.edu');
+$mary = new Person('Realtor', 'Mary', 'Smith', 'marysmith@njit.edu');
+$tony = new Person('Teacher', 'Tony', 'Hawk', 'tonyhawk@njit.edu');
+$johnny = new Person('Unemployed', 'Johnny', 'Applesead', 'johnnyapplesead@njit.edu');
+
+$personArray = array($john, $mary, $tony, $johnny);
+
+echo '<h1>test</h1>';
+//echo $personArray[$john];
+print_r($personArray[0]);
+//echo $personArray[0];
+
+foreach ($personArray as $key => $person)
+{
+	echo $key . '<br />';
+	echo $person->test();
+	echo $person->getTitle() . '<br />';
+	echo $person->getFirstName() . '<br />';
+	echo $person->getLastName() . '<br />';
+	echo $person->getEmailAddress() . '<br /><br />';
+}
+
+echo '<pre>';
+$john->test();
+echo '</pre>';
+
+$whats = $john->test();
+//$whats = get_object_vars($john);
+foreach ($whats as $key => $value)
+{
+	echo 'key: ' . $key . ', value: ' . $value . '<br />';
+}
+
+//cT($whats);
+cT($personArray);
+
+function cT($arrayPeople)
+{
+	$firstObject = $arrayPeople[0];
+	$test = $firstObject->test();
+	?>
+	<table>
+		<tr>
+			<?php
+			foreach ($test as $key => $value)
+			{
+				?>
+				<th><?php echo $key; ?></th>
+				<?php
+			}
+			?>
+		</tr>
+			<?php
+			foreach ($arrayPeople as $key => $value)
+			{
+				?>
+				<tr>
+					<td><?php echo $value->getTitle(); ?></td>
+					<td><?php echo $value->getFirstName(); ?></td>
+					<td><?php echo $value->getLastName(); ?></td>
+					<td><?php echo $value->getEmailAddress(); ?></td>
+				</tr>
+				<?php
+			}
+			?>
+	</table>
+	<?php
+}
+
+
+$class_vars = get_object_vars($john);
+foreach ($class_vars as $name => $value)
+{
+	echo $name . ': ' . $value . '<br />';
+}
+
+//$test = get_object_vars($john);
+//var_dump($test);
+
+echo '<pre>';
+print_r($personArray);
+echo '</pre>';
+
+
+function makeTable($array)
+{
+	echo 'Hello';
+}
+//echo $personArray[0];
+print_r(array_keys($personArray[0]));
+$testing = each($personArray);
+echo '<pre>';
+print_r($testing);
+echo '</pre>';
+
+//var_dump(get_object_vars($personArray));
+	
+
 
 
 ?>
