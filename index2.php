2cca5cdb1d6d4bf7b86044d306a537327e4ad7f0
Tom <tgoralczyk1@gmail.com>
2016-02-10 19:04:19 -0500

    Created a getter and setter for the title attribute

--- index2.php
+++ index2.php
@@ -4,6 +4,17 @@ class Page
 {
 	private $_title;
 	private $_footer;
+	
+	// Getters and setters
+	public function getTitle()
+	{
+		return $this->_title;
+	}
+	
+	public function setTitle($newTitle)
+	{
+		$this->_title = $newTitle;
+	}
 }
 	
 ?>
