c04bac8d9a58a47c9778f27c28aa887d7053e7e4
Tom <tgoralczyk1@gmail.com>
2016-02-15 17:37:04 -0500

    Completed the link class and tested it for correctness and everything seems to work

--- index3.php
+++ index3.php
@@ -1,9 +1,21 @@
 <?php
 
+// Show errors
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+/////////////////////
+
 // Link Class
-class link
+class Link
 {
-  
+  public static function newLink($title, $href, $target)
+  {
+    $link = '<a href="' . $href . '" target ="' . $target . '">' . $title . '</a>';
+    return $link;
+  }
 }
 
+$link = Link::newLink('Google', 'http://www.google.com', '_BLANK');
+echo $link;
 ?>
