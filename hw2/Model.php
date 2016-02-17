dc0527ab0fae815661c01069fe6d0c68aed619b9
Tom <tgoralczyk1@gmail.com>
2016-02-17 09:31:41 -0500

    Separated the Model class from the index file

---
+++ hw2/Model.php
@@ -0,0 +1,24 @@
+<?php
+include_once('autoloadFunction.php');
+
+// Model Class
+class Model
+{
+  private $_guid;
+
+  public function __construct()
+  {
+    // Start the session
+    session_start();
+
+    // Create a unique ID to identify the record
+    $this->_guid = uniqid();
+  }
+
+  public function save()
+  {
+    // Saves the model into the session
+    $_SESSION[$this->_guid] = (array) $this;
+  }
+}
+?>
