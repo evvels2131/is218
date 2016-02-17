4a0f55a5ec7263a26a3a218ccdab61620e42f8c0
Tom <tgoralczyk1@gmail.com>
2016-02-16 19:03:55 -0500

    Created a model class with a function that saves the model into the session

--- hw2/index.php
+++ hw2/index.php
@@ -1,5 +1,21 @@
 <?php
 
+// ModelClass
+class Model
+{
+  private $_guid;
 
+  // Start the session
+  session_start();
+
+  // Create a unique ID to identify the record
+  $this->_guid = uniqid();
+
+  public function save()
+  {
+    // Saves the model into the session
+    $_SESSION[$this->_guid] = (array) $this;
+  }
+}
 
 ?>
