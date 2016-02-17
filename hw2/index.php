331133c33e8ec8b12e294c6d3746f6fa86dc4851
Tom <tgoralczyk1@gmail.com>
2016-02-17 09:33:01 -0500

    Separated the CarView class from the index file

--- hw2/index.php
+++ hw2/index.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once('autoloadFunction.php');
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -15,7 +17,7 @@ $car->setYear('2014');
 // Call the save to store the record in the session
 $car->save();
 
-session_unset();
+//session_unset();
 
 echo '<pre>';
 print_r($_SESSION);
@@ -33,77 +35,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET')
 else
 {
   $obj->post();
-}
-
-// Model Class
-class Model
-{
-  private $_guid;
-
-  public function __construct()
-  {
-    // Start the session
-    session_start();
-
-    // Create a unique ID to identify the record
-    $this->_guid = uniqid();
-  }
-
-  public function save()
-  {
-    // Saves the model into the session
-    $_SESSION[$this->_guid] = (array) $this;
-  }
-}
-
-// CarModel Class
-class CarModel extends Model
-{
-  private $_make;
-  private $_model;
-  private $_year;
-
-  // Getters and setters
-  public function getMake()
-  {
-    return $this->_make;
-  }
-
-  public function setMake($newMake)
-  {
-    $this->_make = $newMake;
-  }
-
-  public function getModel()
-  {
-    return $this->_model;
-  }
-
-  public function setModel($newModel)
-  {
-    $this->_model = $newModel;
-  }
-
-  public function getYear()
-  {
-    return $this->_year;
-  }
-
-  public function setYear($newYear)
-  {
-    $this->_year = $newYear;
-  }
-}
-
-// CarView Class
-class CarView
-{
-  public function __construct($car)
-  {
-    echo 'Make: ' . $car->getMake() . '<br />';
-    echo 'Model: ' . $car->getModel() . '<br />';
-    echo 'Year: ' . $car->getYear() . '<br />';
-  }
 }
 
 // CarController
