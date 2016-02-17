c515f71e13e68ba56da6ee6b9764e0d0dcebba5f
Tom <tgoralczyk1@gmail.com>
2016-02-16 19:19:17 -0500

    Created a CarView class

--- hw2/index.php
+++ hw2/index.php
@@ -4,6 +4,22 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// Instantiate the car model
+$car = new CarModel;
+
+// Set the properties
+$car->setMake('Ford');
+$car->setModel('Taurus');
+
+// Call the save to store the record in the session
+$car->save();
+
+//session_unset();
+
+echo '<pre>';
+print_r($_SESSION);
+echo '</pre>';
+
 // Model Class
 class Model
 {
@@ -25,8 +41,8 @@ class Model
   }
 }
 
-// Car Class
-class Car extends Model
+// CarModel Class
+class CarModel extends Model
 {
   private $_make;
   private $_model;
@@ -50,6 +66,16 @@ class Car extends Model
   public function setModel($newModel)
   {
     $this->_model = $newModel;
+  }
+}
+
+// CarView Class
+class CarView
+{
+  public function __construct($car)
+  {
+    echo 'Make: ' . $car->getMake() . '<br />';
+    echo 'Model: ' . $car->getModel() . '<br />';
   }
 }
 ?>
