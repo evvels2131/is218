dc828c396ef0a630378ddec5e9ed2e3a02c77d96
Tom <tgoralczyk1@gmail.com>
2016-02-18 16:26:38 -0500

    Moved the Form class into a new location

---
+++ hw2/app/html/Form.php
@@ -0,0 +1,38 @@
+<?php
+namespace app\html;
+
+include_once('autoloadFunction.php');
+
+// Form Class
+class Form extends HTML
+{
+  private $_action;
+  private $_method;
+  private $_formHeader;
+  private $_form;
+
+  public function __construct($action, $method)
+  {
+    $this->_action = $action;
+    $this->_method = $method;
+    $this->_formHeader = '<form action="' . $this->_action . '" method="' . $this->_method . '"><br />';
+  }
+
+  public function addNewInput($inputItem)
+  {
+    $this->_form[] = $inputItem;
+  }
+
+  public function getForm()
+  {
+    $formHTML = $this->_formHeader;
+    foreach ($this->_form as $inputItem)
+    {
+      $formHTML .= $inputItem . '<br />';
+    }
+    $formHTML .= '</form>';
+
+    return $formHTML;
+  }
+}
+?>
