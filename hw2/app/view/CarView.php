c1c780df46db44606938322d0c3933101e02622f
Tom <tgoralczyk1@gmail.com>
2016-02-18 16:14:41 -0500

    Moved the CarView file to a different location

---
+++ hw2/app/view/CarView.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\view;
+
+include_once('autoloadFunction.php');
+// CarView Class
+class CarView extends View
+{
+  public function __construct($car)
+  {
+    echo 'Make: ' . $car->getMake() . '<br />';
+    echo 'Model: ' . $car->getModel() . '<br />';
+    echo 'Year: ' . $car->getYear() . '<br />';
+  }
+}
+
+?>
