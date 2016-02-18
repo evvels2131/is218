69e1082d2a355aad0f92763281c8f8815a6c7d9c
Tom <tgoralczyk1@gmail.com>
2016-02-18 16:48:15 -0500

    Improved the autoloader function to work with namespaces

--- hw2/autoloadFunction.php
+++ hw2/autoloadFunction.php
@@ -1,9 +1,21 @@
 <?php
 function my_autoloader($class)
 {
-  include $class . '.php';
+  $ds = DIRECTORY_SEPARATOR;
+  $dir = __DIR__;
+
+  $class = str_replace('\\', $ds, $class);
+
+  $file = "{$dir}{$ds}{$class}.php";
+
+  if (is_readable($file)) require_once $file;
 }
 
 spl_autoload_register('my_autoloader');
 
+/*
+function my_autoloader($class)
+{
+  include $class . '.php';
+}*/
 ?>
