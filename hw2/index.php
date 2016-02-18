ef5be137d3009248dc54d9223720155732c47ae7
Tom <tgoralczyk1@gmail.com>
2016-02-18 16:23:45 -0500

    Changed the index file so that is uses namespaces

--- hw2/index.php
+++ hw2/index.php
@@ -1,4 +1,7 @@
 <?php
+use app\controller\CarController;
+use app\model\CarModel;
+use app\view\CarView;
 
 include_once('autoloadFunction.php');
 
@@ -7,22 +10,28 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // Instantiate the car model
-$car = new CarModel;
+$car = new CarModel();
 
 // Set the properties
 $car->setMake('Ford');
 $car->setModel('Taurus');
 $car->setYear('2014');
 
+$car2 = new CarModel();
+$car2->setMake('Chevy');
+$car2->setModel('Malibu');
+$car2->setYear('2012');
+
 // Call the save to store the record in the session
-//$car->save();
+$car->save();
+$car2->save();
 
 //session_unset();
 
 echo '<pre>';
 print_r($_SESSION);
 echo '</pre>';
-
+/*
 echo '<hr>';
 echo '<b>$_SERVER["REQUEST_METHOD"]</b> --> ';
 echo $_SERVER['REQUEST_METHOD'];
@@ -35,5 +44,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET')
 else
 {
   $obj->post();
-}
+}*/
 ?>
