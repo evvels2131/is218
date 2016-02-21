c59c1d0b69d0d81074f615c429193f290e782ff7
Tom <tgoralczyk1@gmail.com>
2016-02-20 22:06:58 -0500

    Added a new function that cleans the attribute of an object from unnecessary data

--- hw2/app/html/HTML.php
+++ hw2/app/html/HTML.php
@@ -3,6 +3,15 @@ namespace app\html;
 
 abstract class HTML
 {
+  public static function cleanAttribute($attribute)
+  {
+    $pos = strrpos($attribute, '_') + 1;
+    $strlen = strlen($attribute) - 1;
+    $cleanAttribute = substr($attribute, $pos, $strlen);
+    $cleanAttribute = ucfirst($cleanAttribute);
+
+    return $cleanAttribute;
+  }
 
 }
 
