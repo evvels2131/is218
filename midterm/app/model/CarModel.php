22e99e386edcc8e7b60ef07726c031013e026eaf
Tom <tgoralczyk1@gmail.com>
2016-02-23 16:35:59 -0500

    Created a getter and setter for the make property

--- midterm/app/model/CarModel.php
+++ midterm/app/model/CarModel.php
@@ -3,6 +3,19 @@ namespace app\model;
 
 class CarModel extends Model
 {
-  
+  private $_make;
+  private $_model;
+  private $_year;
+
+  // Getters and setters
+  public function getMake()
+  {
+    return $this->_make;
+  }
+
+  public function setMake($newMake)
+  {
+    $this->_make = $newMake;
+  }
 }
 ?>
