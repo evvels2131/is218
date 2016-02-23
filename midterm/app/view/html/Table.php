d3b018a61beb9e120e8685a498c2653f491db33e
Tom <tgoralczyk1@gmail.com>
2016-02-23 17:32:22 -0500

    Created a table class that represents an html table

---
+++ midterm/app/view/html/Table.php
@@ -0,0 +1,67 @@
+<?php
+namespace app\view\html;
+
+class Table extends HTML
+{
+  public static function generateTable($array)
+  {
+    if (is_array($array))
+    {
+      $i = 0;
+      $find = 'id'; // ID should be generated as links
+      $htmlTABLE = '<table><tr>';
+
+      // Get table headings
+      foreach ($array as $key => $innerArray)
+      {
+        if (is_array($innerArray))
+        {
+          foreach ($innerArray as $attribute => $value)
+          {
+            if (strpos($attribute, $find) !== false)
+            {
+              $htmlTABLE .= '<th>Action</th>';
+            }
+            else
+            {
+              $clean = HTML::cleanAttribute($attribute);
+              $htmlTABLE .= '<th>' . $clean . '</th>';
+            }
+          }
+
+          if (++$i == 1)
+          {
+            break;
+          }
+        }
+      }
+      $htmlTABLE .= '</tr>';
+
+      // Get the rest of the table
+      foreach ($array as $key => $innerArray)
+      {
+        if (is_array($innerArray))
+        {
+          $htmlTABLE .= '<tr>';
+          foreach ($innerArray as $attribute => $value)
+          {
+            if (strpos($attribute, $find) !== false)
+            {
+              $href = 'index.php?page=car&id=' . $value;
+              $htmlTABLE .= '<td>' . Link::newLink('View', $href, '_self') . '</td>';
+            }
+            else
+            {
+              $htmlTABLE .= '<td>' . $value . '</td>';
+            }
+          }
+          $htmlTABLE .= '</tr>';
+        }
+      }
+      $htmlTABLE .= '</table>';
+
+      return $htmlTABLE;
+    }
+  }
+}
+?>
