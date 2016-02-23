76d9006e38e26abed926ccfa8fcb00f2f279e0af
Tom <tgoralczyk1@gmail.com>
2016-02-23 17:39:14 -0500

    Created a new class for AboutPage

---
+++ midterm/app/view/page/AboutPage.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\view\page;
+
+use app\view\html\Paragraph;
+
+class AboutPage extends Page
+{
+  public function get()
+  {
+    Paragraph::newParagraph('This is the AboutPage. get() method called.');
+  }
+
+  public function post()
+  {
+    Paragraph::newParagraph('This is the AboutPage. post() method called.');
+  }
+}
+
+?>
