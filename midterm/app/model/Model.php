f6b3da42069b6b55227b87ce56aa6b690979d7e9
Tom <tgoralczyk1@gmail.com>
2016-02-23 14:07:55 -0500

    Created a construct function that creates a session and assigns a unique id to each model object

--- midterm/app/model/Model.php
+++ midterm/app/model/Model.php
@@ -3,6 +3,26 @@ namespace app\model;
 
 abstract class Model
 {
-  
+  private $_guid;
+
+  public function __construct($guid = "")
+  {
+    // Start the session if it's not set
+    if (!isset($_SESSION))
+    {
+      session_start();
+    }
+
+    // Check if guid passed as a parameter
+    if (!empty($guid))
+    {
+      $this->_guid = $guid;
+    }
+    else
+    {
+      // Create a unique ID to identify the record
+      $this->_guid = uniqid();
+    }
+  }
 }
 ?>
