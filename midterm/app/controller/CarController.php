d66545bbc7c5ce788abe3be8d14e06ee4d0c13d8
Tom <tgoralczyk1@gmail.com>
2016-02-24 09:09:34 -0500

    Implemented the post function

--- midterm/app/controller/CarController.php
+++ midterm/app/controller/CarController.php
@@ -4,12 +4,23 @@ namespace app\controller;
 use app\view\page\AboutPage;
 use app\view\page\HomePage;
 use app\view\page\AddCarPage;
+use app\model\CarModel;
 
 class CarController extends Controller
 {
   public function post()
   {
+    $make   = $_POST['make'];
+    $model  = $_POST['model'];
+    $year   = $_POST['year'];
 
+    $car = new CarModel;
+
+    $car->setMake($make);
+    $car->setModel($model);
+    $car->setYear($year);
+
+    $car->save();
   }
 
   public function get($page = "")
