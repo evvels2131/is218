6e5e7c93bf41c62504e7b4b01d25b78525889ec9
Tom <tgoralczyk1@gmail.com>
2016-02-23 20:27:32 -0500

    Added functionality to the get function to request proper pages

--- midterm/app/controller/CarController.php
+++ midterm/app/controller/CarController.php
@@ -1,16 +1,37 @@
 <?php
 namespace app\controller;
 
+use app\view\page\AboutPage;
+use app\view\page\HomePage;
+use app\view\page\AddCarPage;
+
 class CarController extends Controller
 {
-  public static function post()
+  public function post()
   {
 
   }
 
-  public static function get()
+  public function get($page = "")
   {
-
+    if (!empty($page))
+    {
+      if ($page == 'about')
+      {
+        $about = new AboutPage;
+        $about->get();
+      }
+      else if ($page == 'addcar')
+      {
+        $addCar = new AddCarPage;
+        $addCar->get();
+      }
+    }
+    else
+    {
+      $home = new HomePage;
+      $home->get();
+    }
   }
 }
 
