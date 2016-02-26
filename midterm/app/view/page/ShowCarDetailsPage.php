c432c17754886cd2eefdf5fc4d24bc587d269f39
Tom <tgoralczyk1@gmail.com>
2016-02-26 14:33:22 -0500

    Minor change

--- midterm/app/view/page/ShowCarDetailsPage.php
+++ midterm/app/view/page/ShowCarDetailsPage.php
@@ -22,26 +22,32 @@ class ShowCarDetailsPage extends Page
       echo '<b>' . $clean . '</b>: ' . $value . '<br />';
     }
 
-    echo Heading::newHeading('h2', 'Edit the car below');
+    echo Heading::newHeading('h2', 'Edit or delete the car below');
+
     $form = new Form('index.php', 'POST');
     foreach ($car as $attribute => $value)
     {
-      $clean = HTML::cleanAttributeForm($attribute);
+      $clean = HTML::cleanAttribute($attribute, 'false');
+
       if ($clean == 'guid')
       {
-        break;
+        // Disable the GUID input field so it cannot be edited
+        $$clean = InputField::newInputField('text', $clean, $value, 'readonly');
+        $form->addNewInput($$clean);
       }
       else
       {
+        // If not a GUID input field, allow for editing
         $$clean = InputField::newInputField('text', $clean, $value);
         $form->addNewInput($$clean);
       }
     }
-    $submit = InputField::newInputField('submit', '', 'Save');
-    $form->addNewInput($submit);
+    $save = InputField::newInputField('submit', 'save', 'Save');
+    $delete = InputField::newInputField('submit', 'delete', 'Delete');
+    $form->addNewInput($save);
+    $form->addNewInput($delete);
 
     echo $form->getForm();
-
   }
 }
 
