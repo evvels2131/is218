543d7ff7bf3d8062b194382e13fb00d940cfb7f3
Tom <tgoralczyk1@gmail.com>
2016-02-26 16:46:40 -0500

    Homepage now displays table if data is available or a message saying to add cars

--- midterm/app/view/page/HomePage.php
+++ midterm/app/view/page/HomePage.php
@@ -3,13 +3,29 @@ namespace app\view\page;
 
 use app\view\html\Paragraph;
 use app\view\html\Heading;
+use app\view\html\Table;
 
 class HomePage extends Page
 {
-  public function __construct()
+  public function __construct($array = '')
   {
-    echo Heading::newHeading('h1', 'Home Page');
-    echo Paragraph::newParagraph('Sample paragraph');
+    //$array = '';
+    echo Heading::newHeading('h1', 'Midterm Project - IS 218-002');
+    echo Heading::newHeading('h2', 'Tomasz Goralczyk');
+    echo '<hr>';
+
+    if (!empty($array))
+    {
+      echo Heading::newHeading('h3', 'Cars Stored in Session');
+      echo Table::generateTable($array);
+    }
+    else
+    {
+      echo Heading::newHeading('h3', 'No cars stored in session to be
+        displayed.');
+      echo Heading::newHeading('h3', 'Add a new car by clicking the
+        "Add New Car" link above.');
+    }
   }
 }
 ?>
