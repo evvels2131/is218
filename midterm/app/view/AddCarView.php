08d93d553fb4de6814e059b2347a2320040c7322
Tom <tgoralczyk1@gmail.com>
2016-02-25 20:04:07 -0500

    Created a new AddCarView class taht calls the AddCarPage class which returns the page to be displayed

---
+++ midterm/app/view/AddCarView.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\view;
+
+use app\view\page\AddCarPage;
+
+class AddCarView extends View
+{
+  public function __construct()
+  {
+    $addCarPage = new AddCarPage();
+  }
+}
+?>
