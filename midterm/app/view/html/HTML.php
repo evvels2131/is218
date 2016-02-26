dca07a8297b72d4ba023cda409d560406d28d718
Tom <tgoralczyk1@gmail.com>
2016-02-26 14:14:51 -0500

    Combined two nearly identical functions into one

--- midterm/app/view/html/HTML.php
+++ midterm/app/view/html/HTML.php
@@ -3,23 +3,21 @@ namespace app\view\html;
 
 abstract class HTML
 {
-  public static function cleanAttribute($attribute)
+  public static function cleanAttribute($attribute, $capital = '')
   {
     $pos = strrpos($attribute, '_') + 1;
     $strlen = strlen($attribute) - 1;
-    $cleanAttribute = ucfirst(substr($attribute, $pos, $strlen));
 
-    return $cleanAttribute;
-  }
-
-  public static function cleanAttributeForm($attribute)
-  {
-    $pos = strrpos($attribute, '_') + 1;
-    $strlen = strlen($attribute) - 1;
-    $cleanAttribute = substr($attribute, $pos, $strlen);
+    if (empty($capital))
+    {
+      $cleanAttribute = ucfirst(substr($attribute, $pos, $strlen));
+    }
+    else
+    {
+      $cleanAttribute = substr($attribute, $pos, $strlen);
+    }
 
     return $cleanAttribute;
   }
 }
-
 ?>
