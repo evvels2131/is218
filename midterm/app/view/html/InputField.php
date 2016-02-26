a05c67543c08ac634202fe660b771b39c397c9ea
Tom <tgoralczyk1@gmail.com>
2016-02-26 14:30:56 -0500

    Updated the InputField class to allow to distinguish between placeholder and value attributes

--- midterm/app/view/html/InputField.php
+++ midterm/app/view/html/InputField.php
@@ -3,15 +3,22 @@ namespace app\view\html;
 
 class InputField extends HTML
 {
-  public static function newInputField($type, $name = '', $value, $disabled = '')
+  public static function newInputField($type, $name = '', $value,
+    $readonly = '', $placeholder = '')
   {
     if (empty($name) && empty($disabled))
     {
       $input = '<input type="' . $type . '" value="' . $value .  '"><br />';
     }
+    else if (!empty($placeholder))
+    {
+      $input = '<input type="' . $type . '" name="' . $name . '" placeholder="' .
+        $value .  '"' . $readonly . '><br />';
+    }
     else
     {
-      $input = '<input type="' . $type . '" name="' . $name . '" value="' . $value .  '"' . $disabled . '><br />';
+      $input = '<input type="' . $type . '" name="' . $name . '" value="' .
+        $value .  '"' . $readonly . '><br />';
     }
 
     return $input;
