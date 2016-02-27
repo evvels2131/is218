7636d8b2eea22c0443a010e1a3c1fb5c822af2b1
Tom <tgoralczyk1@gmail.com>
2016-02-27 16:13:59 -0500

    Improved input fields to work with diffrent types of input fields with different attributes

--- midterm/app/view/html/InputField.php
+++ midterm/app/view/html/InputField.php
@@ -6,22 +6,28 @@ class InputField extends HTML
   public static function newInputField($type, $name = '', $value,
     $readonly = '', $placeholder = '')
   {
-    if (empty($name) && empty($disabled))
+    if (!empty($readonly))
     {
-      $input = '<input type="' . $type . '" value="' . $value .  '"><br />';
+      $input = '<input type="' . $type . '" name="' . $name . '"
+        value="' . $value . '" readonly>';
     }
     else if (!empty($placeholder))
     {
-      $input = '<input type="' . $type . '" name="' . $name . '" placeholder="' .
-        $value .  '"' . $readonly . '><br />';
+      $input = '<input type="' . $type . '" name="' . $name . '"
+        placeholder="' . $placeholder .'">';
+    }
+    else if (!empty($name))
+    {
+      $input = '<input type="' . $type . '" name="' . $name . '"
+        value="' . $value . '">';
     }
     else
     {
-      $input = '<input type="' . $type . '" name="' . $name . '" value="' .
-        $value .  '"' . $readonly . '><br />';
+      $input = '<input type="' . $type . '" value="' . $value . '">';
     }
 
     return $input;
   }
 }
+
 ?>
