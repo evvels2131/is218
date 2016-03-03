f613217dfd68827a5e353bde97284126b48d2848
Tom <tgoralczyk1@gmail.com>
2016-03-03 17:24:57 -0500

    Added a paragraph class that generates html paragraphs

---
+++ final/app/view/html/Paragraph.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\view\html;
+
+class Paragraph extends HTML
+{
+  public static function newParagraph($text)
+  {
+    $paragraph = '<p>' . $text . '</p>';
+
+    return $paragraph;
+  }
+}
+
+?>
