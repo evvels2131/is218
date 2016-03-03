a626606e9ecb84a50d4b639b3c324285e98f1926
Tom <tgoralczyk1@gmail.com>
2016-03-03 12:12:59 -0500

    Minor change

--- midterm/app/view/page/AddCarPage.php
+++ midterm/app/view/page/AddCarPage.php
@@ -11,8 +11,8 @@ class AddCarPage extends Page
   {
     // Get header
     echo parent::getHeader('New Car');
-    
-    echo parent::alertDiv('success', Heading::newHeading('h3', 'Add a New Car'));
+
+    echo parent::alertDiv('warning', Heading::newHeading('h4', 'Add a New Car'));
 
     $make   = InputField::newInputField('text', 'make', 'Make', '', 'Make');
     $model  = InputField::newInputField('text', 'model', 'Model', '', 'Model');
