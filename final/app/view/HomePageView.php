1d4f1924e831bf8b7930f7bf843dd6864e6763af
Tom <tgoralczyk1@gmail.com>
2016-03-03 14:54:19 -0500

    Homepage displays a welcome message if the session is set, otherwise displays a login form

--- final/app/view/HomePageView.php
+++ final/app/view/HomePageView.php
@@ -1,11 +1,30 @@
 <?php
 namespace app\view;
 
+use app\view\html\Form;
+use app\view\html\InputField;
+
 class HomePageView extends View
 {
   public function __construct($data = '')
   {
-    echo 'Homepage';
+    echo 'Homepage<br /><br />';
+    if (isset($_SESSION))
+    {
+      echo 'Welcome <strong>' . $_SESSION['username'] . '</strong>!';
+    }
+    else
+    {
+      $username = InputField::newInputField('text', 'username', 'Username');
+      $password = InputField::newInputField('password', 'password', 'Password');
+      $submit   = InputField::newInputField('submit', 'login', '', 'Log In');
+
+      $form = new Form('index.php', 'POST');
+      $form->addNewInput($username);
+      $form->addNewInput($password);
+      $form->addNewInput($submit);
+      echo $form->getForm();
+    }
   }
 }
 
