37b4ca7cf9e18038021a3623ca78664ca3bc32d2
Tom <tgoralczyk1@gmail.com>
2016-03-03 13:40:25 -0500

    Implemented the autoloading function

---
+++ final/autoloadFunction.php
@@ -0,0 +1,17 @@
+<?php
+
+function my_autoloader($class)
+{
+  $ds = DIRECTORY_SEPARATOR;
+  $dir = __DIR__;
+
+  $class = str_replace('\\', $ds, $class);
+
+  $file = "{$dir}{$ds}{$class}.php";
+
+  if (is_readable($file)) require_once $file;
+}
+
+spl_autoload_register('my_autoloader');
+
+?>
