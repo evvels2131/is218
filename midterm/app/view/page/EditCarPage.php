ca97c5701e44e439ead0046504c44359dbfb5735
Tom <tgoralczyk1@gmail.com>
2016-03-03 12:10:16 -0500

    Created a EditCarPage class that displays the form with the car information to be edited

---
+++ midterm/app/view/page/EditCarPage.php
@@ -0,0 +1,54 @@
+<?php
+namespace app\view\page;
+
+use app\view\html\HTML;
+use app\view\html\InputField;
+use app\view\html\Form;
+use app\view\html\Heading;
+
+class EditCarPage extends Page
+{
+  public function __construct($array = '', $carID = '')
+  {
+    // Get header
+    echo parent::getHeader('Edit Car');
+
+    // Get the proper car in the array
+    $car = $array[$carID['id']];
+
+    echo '<br />';
+    echo parent::alertDiv('warning', Heading::newHeading('h4', 'Edit or delete the car below'));
+
+    $form = new Form('index.php', 'POST');
+    foreach ($car as $attribute => $value)
+    {
+      $clean = HTML::cleanAttribute($attribute, 'false');
+
+      if ($clean == 'guid')
+      {
+        // Disable the GUID input field so it cannot be edited
+        $$clean = InputField::newInputField('text', $clean, $value, 'readonly');
+        $form->addNewInput($$clean);
+      }
+      else
+      {
+        // If not a GUID input field, allow for editing
+        $$clean = InputField::newInputField('text', $clean, $value);
+        $form->addNewInput($$clean);
+      }
+    }
+    $save   = InputField::newInputField('submit', 'save', 'Save');
+    $delete = InputField::newInputField('submit', 'delete', 'Delete');
+    $form->addNewInput($save);
+    $form->addNewInput($delete);
+
+    echo parent::formDiv($form->getForm());
+
+    // Get footer
+    echo parent::getFooter();
+  }
+}
+
+
+
+?>
