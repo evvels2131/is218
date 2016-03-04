1a62e573027b9b1b5bf93609bed5e0cc2f958b15
Tom <tgoralczyk1@gmail.com>
2016-03-04 17:49:01 -0500

    Testing the newly created classes and functions

--- final/index.php
+++ final/index.php
@@ -1,13 +1,37 @@
 <?php
-session_start();
+//session_start();
 
 use app\App;
+use app\model\Database;
+use app\model\Model;
 
 require_once('autoloadFunction.php');
 
-$app = new App;
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-$_SESSION['username'] = 'tgoralczyk1';
+//print_r($_SESSION);
+//$app = new App;
+
+//$_SESSION['username'] = 'tgoralczyk1';
 //session_unset();
+
+
+$db = new Database;
+$db->register('Jimmy', 'jimmy@gmail.com', '102kgku22');
+$db->register('Brian', 'brian@gmail.com', '213sgku22');
+
+$mdb = new Model;
+$mdb->registerNewUser('Marcin', 'marcing@gmail.com', '102103');
+echo '<pre>';
+print_r($db->getUsers());
+echo '</pre>';
+//$db->register('Steve', 'steve@gmail.com', '1291931');
+//print_r($db->getUsers());
+
+
+
+
 
 ?>
