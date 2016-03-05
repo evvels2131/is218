386ff65af3be105557d231006227ca3440f2181f
Tom <tgoralczyk1@gmail.com>
2016-03-04 21:43:35 -0500

    Minor changes to the home page view

--- final/app/view/HomePageView.php
+++ final/app/view/HomePageView.php
@@ -4,12 +4,15 @@ namespace app\view;
 use app\view\html\Form;
 use app\view\html\InputField;
 use app\view\html\Button;
+use app\view\html\Paragraph;
+use app\view\html\Link;
+use app\view\html\Heading;
 
 class HomePageView extends View
 {
   public function __construct($data = '')
   {
-    //echo 'Homepage<br /><br />';
+    echo parent::htmlHeader('Home');
     $content = '';
     if (isset($_SESSION))
     {
@@ -17,9 +20,13 @@ class HomePageView extends View
     }
     else
     {
+      $heading = parent::htmlAlertDiv('info', Heading::newHeading('h4', '<b>Welcome!</b>
+        Sing in or sing up below'));
+      echo parent::htmlDiv($heading, 8);
+
       $username = InputField::newInputField('text', 'username', 'Username');
       $password = InputField::newInputField('password', 'password', 'Password');
-      $submit   = InputField::newInputField('submit', 'login', '', 'Log In');
+      $submit   = Button::newButton('submit', 'btn-primary', 'Sing in');
 
       $form = new Form('index.php', 'POST');
       $form->addNewInput($username);
@@ -27,11 +34,12 @@ class HomePageView extends View
       $form->addNewInput($submit);
 
       $content .= $form->getForm();
+
+      $content .= Paragraph::newParagraph('Not a member yet? Please sign up below!');
+      $content .= Link::newLink('Sing up', 'index.php?page=signup', '_self');
     }
 
-    //$content .= Button::newButton('submit', 'btn-info', 'Information');
     // Generate HTML and display content
-    echo parent::htmlHeader('Home');
     echo parent::htmlDiv($content, 6);
     echo parent::htmlFooter();
   }
