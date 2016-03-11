ec118da04c1314091a5ef3e01df219d203ff444e
Tom <tgoralczyk1@gmail.com>
2016-03-11 11:35:06 -0500

    Created an if statement that will differentiate between two different types of a button that could be added to a form

--- hw3/app/view/html/Button.php
+++ hw3/app/view/html/Button.php
@@ -5,8 +5,16 @@ class Button extends HTML
 {
   public static function newButton($type, $name = '', $class, $text)
   {
-    $buttonHTML = '<button type="' . $type . '" name="' . $name . '"
-      class="btn ' . $class . '">' . $text . '</button>';
+    if (!empty($type) && !empty($name) && !empty($class) && !empty($text))
+    {
+      $buttonHTML = '<button type="' . $type . '" name="' . $name . '"
+        class="btn btn-' . $class . '">' . $text . '</button>';
+    }
+    else
+    {
+      $buttonHTML = '<button type="' . $type . '" class="btn btn-' . $class . '">'
+        . $text . '</button>';
+    }
 
     return $buttonHTML;
   }
