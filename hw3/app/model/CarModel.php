267f5c8722b88e4573a2a5bd433e69ba562549b9
Tom <tgoralczyk1@gmail.com>
2016-03-11 15:51:29 -0500

    Added a new property called image which holds the path to the image that will be displayed as a thumbnail

--- hw3/app/model/CarModel.php
+++ hw3/app/model/CarModel.php
@@ -6,6 +6,7 @@ class CarModel extends Model
   private $_make;
   private $_model;
   private $_year;
+  private $_image;
 
   // Getters and setters
   public function getMake()
@@ -36,6 +37,16 @@ class CarModel extends Model
   public function setYear($newYear)
   {
     $this->_year = $newYear;
+  }
+
+  public function getImage()
+  {
+    return $this->_image;
+  }
+
+  public function setImage($image)
+  {
+    $this->_image = $image;
   }
 }
 ?>
