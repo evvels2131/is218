de91d09412ebaf4e096bb4f4d42cfa5730fcce1e
Tom <tgoralczyk1@gmail.com>
2016-03-11 12:00:36 -0500

    Added the ability to create file ine type input fields

--- hw3/app/view/html/InputField.php
+++ hw3/app/view/html/InputField.php
@@ -6,19 +6,23 @@ class InputField extends HTML
   public static function newInputField($type, $name = '', $value = '',
     $readonly = '', $placeholder = '')
   {
-    if (!empty($type) && !empty($name) && !empty($value) && !empty($readonly))
+    if ($type == 'file')
+    {
+      $input = '<input type="file" id="' . $name . '" placeholder="' . $placeholder . '">';
+    }
+    else if (!empty($type) && !empty($name) && !empty($value) && !empty($readonly))
     {
       // Used for the ID attribute
       $input = '<input type="' . $type . '" name="' . $name . '" class="form-control"
         value="' . $value . '" placeholder="ID" readonly>';
     }
-    else if (!empty($type) && !empty($name) && !empty($value))
+    else if (!empty($type) && !empty($name) && !empty($value) && empty($placeholder))
     {
       // Used for make, model, and year [Edit Car Page]
       $input = '<input type="' . $type . '" class="form-control"
         value="' . $value . '" name="' . $name . '">';
     }
-    else if (!empty($type) && !empty($name) && !empty($placeholder))
+    else if (!empty($type) && !empty($name) && !empty($placeholder) && empty($value))
     {
       // AddCarPage.php
       $input = '<input type="' . $type . '" name="' . $name . '" class="form-control"
