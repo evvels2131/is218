d2d5a7fde08c8090e642c1fea966f63128fd4509
Tom <tgoralczyk1@gmail.com>
2016-03-11 11:36:16 -0500

    Rewrote the InputField class to better handle different types of input fields

--- hw3/app/view/html/InputField.php
+++ hw3/app/view/html/InputField.php
@@ -3,44 +3,26 @@ namespace app\view\html;
 
 class InputField extends HTML
 {
-  public static function newInputField($type, $name = '', $value,
+  public static function newInputField($type, $name = '', $value = '',
     $readonly = '', $placeholder = '')
   {
-    if ($type == 'text')
+    if (!empty($type) && !empty($name) && !empty($value) && !empty($readonly))
     {
-      if (!empty($readonly))
-      {
-        $input = '<input type="' . $type . '" name="' . $name . '"
-          class="form-control" value="' . $value . '" placeholder="ID" readonly >';
-      }
-      else if (!empty($placeholder))
-      {
-        $input = '<input type="' . $type . '" name="' . $name . '"
-          class="form-control" placeholder="' . $placeholder .'">';
-      }
-      else if (!empty($name))
-      {
-        $input = '<input type="' . $type . '" name="' . $name . '"
-          class="form-control" value="' . $value . '" >';
-      }
-      else
-      {
-        $input = '<input type="' . $type . '" class="form-control">
-          value="' . $value . '"';
-      }
+      // Used for the ID attribute
+      $input = '<input type="' . $type . '" name="' . $name . '" class="form-control"
+        value="' . $value . '" placeholder="ID" readonly>';
     }
-    else
+    else if (!empty($type) && !empty($name) && !empty($value))
     {
-      if (!empty($name))
-      {
-        $input = '<button type="' . $type . '" name="' . $name . '"
-          value="' . $value . '" class="btn btn-primary">' . $value . '</button>';
-      }
-      else
-      {
-        $input = '<button type="' . $type . '" value="' . $value . '"
-          class="btn btn-primary">' . $value . '</button>';
-      }
+      // Used for make, model, and year [Edit Car Page]
+      $input = '<input type="' . $type . '" class="form-control"
+        value="' . $value . '" name="' . $name . '">';
+    }
+    else if (!empty($type) && !empty($name) && !empty($placeholder))
+    {
+      // AddCarPage.php
+      $input = '<input type="' . $type . '" name="' . $name . '" class="form-control"
+        placeholder="' . $placeholder . '">';
     }
 
     return $input;
