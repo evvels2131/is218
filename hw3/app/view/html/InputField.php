8e6f4df9984b564c2907d521d45ed6d6097bbe42
Tom <tgoralczyk1@gmail.com>
2016-03-11 19:14:58 -0500

    Minor change to work better with the edit car page

--- hw3/app/view/html/InputField.php
+++ hw3/app/view/html/InputField.php
@@ -10,11 +10,12 @@ class InputField extends HTML
     {
       $input = '<input type="file" name="' . $name . '" placeholder="' . $placeholder . '">';
     }
-    else if (!empty($type) && !empty($name) && !empty($value) && !empty($readonly))
+    else if (!empty($type) && !empty($name) && !empty($value) && !empty($readonly)
+      && !empty($placeholder))
     {
       // Used for the ID attribute
       $input = '<input type="' . $type . '" name="' . $name . '" class="form-control"
-        value="' . $value . '" placeholder="ID" readonly>';
+        value="' . $value . '" placeholder="' . $placeholder . '" readonly>';
     }
     else if (!empty($type) && !empty($name) && !empty($value) && empty($placeholder))
     {
