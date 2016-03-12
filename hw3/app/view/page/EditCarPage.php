9ce9d0e10747d4ce7c47c9e429c30bcb79d29cf7
Tom <tgoralczyk1@gmail.com>
2016-03-11 19:15:27 -0500

    The edit car page now displays the car image and allows to update the image if the users wishes to do so

--- hw3/app/view/page/EditCarPage.php
+++ hw3/app/view/page/EditCarPage.php
@@ -32,12 +32,22 @@ class EditCarPage extends Page
       if ($clean == 'guid')
       {
         // Disable the GUID input field so it cannot be edited
-        $$clean = InputField::newInputField('text', $clean, $value, 'readonly', '');
+        $$clean = InputField::newInputField('text', $clean, $value, 'readonly', 'ID');
         $form->addNewInput($$clean);
       }
       else if ($clean == 'image')
       {
-        $image = '<img src="' . $value . '" alt="image" class="img-thumbnail">';
+        $img = Heading::newHeading('h2', 'Image');
+        if (!empty($value))
+        {
+          $img .= '<img src="' . $value . '" alt="image" class="img-thumbnail">';
+          $$clean = InputField::newInputField('text', $clean, $value, 'readonly', 'Image Path');
+          $form->addNewInput($$clean);
+        }
+        else
+        {
+          $img .= Heading::newHeading('h4', 'Not available');
+        }
       }
       else
       {
@@ -46,15 +56,17 @@ class EditCarPage extends Page
         $form->addNewInput($$clean);
       }
     }
+    $carPic = InputField::newInputField('file', 'file', '', '', 'New Picture');
     $save   = Button::newButton('submit', 'save', 'success', 'Save');
     $delete = Button::newButton('submit', 'delete', 'danger', 'Delete');
+    $form->addNewInput($carPic);
     $form->addNewInput($save);
     $form->addNewInput($delete);
 
     $content = $form->getForm();
     $content .= Link::newLink('Go Back', 'index.php', '_self') . '</li></ul>';
 
-    echo parent::htmlDiv($image, 6);
+    echo parent::htmlDiv($img, 6);
     echo parent::htmlDiv($content, 4);
 
 
