2ed9d2c3820da98c274ce2532b1fe05b6fba5389
Tom <tgoralczyk1@gmail.com>
2016-03-11 19:17:07 -0500

    Commented the debugging information

--- hw3/index.php
+++ hw3/index.php
@@ -7,6 +7,8 @@ require_once('autoloadFn.php');
 
 $app = new App;
 
+/*
+
 // turn on php error reporting
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -28,5 +30,13 @@ if (isset($_GET) && !empty($_SESSION))
   echo '</pre>';
 }
 
-echo '<b>$_SERVER["REQUEST_METHOD"] --> ' . $_SERVER['REQUEST_METHOD'];
+if (isset($_FILES) && !empty($_FILES))
+{
+  echo '<h2>$_FILES</h2>';
+  echo '<pre>';
+  print_r($_FILES);
+  echo '</pre>';
+}
+
+echo '<b>$_SERVER["REQUEST_METHOD"] --> ' . $_SERVER['REQUEST_METHOD'];*/
 ?>
