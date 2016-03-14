80363985f814a682049ebf7bec11a4a44c56a101
Tom <tgoralczyk1@gmail.com>
2016-03-14 10:39:30 -0400

    Moved the content of the HomePage class to the HomePageView

--- hw3/app/view/HomePageView.php
+++ hw3/app/view/HomePageView.php
@@ -1,13 +1,40 @@
 <?php
 namespace app\view;
 
-use app\view\page\HomePage;
+use app\view\html\Paragraph;
+use app\view\html\Heading;
+use app\view\html\Table;
 
 class HomePageView extends View
 {
   public function __construct($array = '')
   {
-    $homePage = new HomePage($array);
+    // Header
+    echo parent::getHeader('Home');
+
+    // Content
+    if (!empty($array))
+    {
+      $heading = Heading::newHeading('h5', 'Cars Stored in Session');
+      $content = parent::htmlAlertDiv('warning', $heading);
+      echo parent::htmlDiv($content, 8);
+
+      $content = Table::generateTable($array);
+      echo parent::htmlDiv($content, 6);
+    }
+    else
+    {
+      $heading = Heading::newHeading('h5', 'No cars stored in session to be displayed.');
+      $content = parent::htmlAlertDiv('danger', $heading);
+      $heading = Heading::newHEading('h5', 'Add a new car by clicking the <strong>"Add
+        New Car"</strong> link above.');
+      $content .= parent::htmlAlertDiv('info', $heading);
+
+      echo parent::htmlDiv($content, 8);
+    }
+
+    // Footer
+    echo parent::getFooter();
   }
 }
 
