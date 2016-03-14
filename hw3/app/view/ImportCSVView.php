070ffa70a390198e6a3eb51a936aea1d2058fbfc
Tom <tgoralczyk1@gmail.com>
2016-03-14 13:17:42 -0400

    Made some minor changes to the CSVView class so its more presentable

--- hw3/app/view/ImportCSVView.php
+++ hw3/app/view/ImportCSVView.php
@@ -16,24 +16,27 @@ class ImportCSVView extends View
     // Header
     echo parent::getHeader('Import CSV File');
 
-    $content = parent::htmlAlertDiv('warning', Heading::newHeading('h5', 'Import CSV File'));
-    echo parent::htmlDiv($content, 8);
-
     // Check if a csv file exists in the uploads folder
     // If so, display a table and a button to delete the file if necessary
     $dir = 'uploads/*.csv';
 
     foreach (glob($dir) as $file)
     {
+      // If a csv file exists on the server, process it
       if ($file != NULL)
       {
-        $content = Heading::newHeading('h3', 'File currently being processed:');
-        $content .= '<p><strong>' . basename($file) . '</strong></p>';
-        $content .= '<a href="index.php?page=importcsv&deleteFile=' . $file . '"
-          class="btn btn-danger btn-xs" role="button">Remove</a>';
+        $heading = Heading::newHeading('h5', 'File Being Processed');
+        $content = parent::htmlAlertDiv('warning', $heading);
+        
+        echo parent::htmlDiv($content, 8);
+
+        $content = '<p><strong>' . basename($file) . '</strong>&ensp;&ensp;';
+        $content .= '<span><a href="index.php?page=importcsv&deleteFile=' . $file . '"
+          class="btn btn-danger btn-xs" role="button">Remove</a></span></p>';
 
         echo parent::htmlDiv($content, 6);
 
+        // Open up the file and save its content into an array
         $arrayCSV = array();
         $pathToFile = 'uploads/' . basename($file);
         $handle = fopen($pathToFile, 'r');
@@ -50,23 +53,28 @@ class ImportCSVView extends View
         $table .= Heading::newHeading('h3', 'This table only shows 13 columns and 200 rows');
         $table .= '<br />';
         $table .= CSVTable::generateCSVTable($arrayCSV);
-        echo parent::htmlDiv($table, 10);
+
+        echo parent::htmlDiv($table, 12);
       }
     }
 
     // If no csv files in the uploads directory, display the form
     if (glob($dir) == NULL)
     {
+      $heading = Heading::newHeading('h5', 'Upload a CSV file to see its content below
+        in a table.');
+      $content .= parent::htmlAlertDiv('info', $heading);
+      echo parent::htmlDiv($content, 8);
+
       // Form to upload CSV file
       $csvFile = InputField::newInputField('file', 'file', '', '', 'File Input');
-      $submit = Button::newButton('submit', '', 'primary btn-sm', 'Upload');
+      $submit = Button::newButton('submit', '', 'primary btn-xs', 'Upload');
 
       $form = new Form('index.php?page=importcsv', 'POST');
       $form->addNewInput($csvFile);
       $form->addNewInput($submit);
 
-      $content = Heading::newHeading('h4', 'Import CSV File Below:');
-      $content .= $form->getForm();
+      $content = $form->getForm();
       echo parent::htmlDiv($content, 4);
     }
 
