2a166bc7aa9de9df5b925c236371def8c0b2b43f
Tom <tgoralczyk1@gmail.com>
2016-03-29 16:11:01 -0400

    Finished all the examples in the tutorial

--- hw4/index.php
+++ hw4/index.php
@@ -1,5 +1,29 @@
 <?php
 
+//phpinfo();
 
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, 'http://www.google.co.in');
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+$result = curl_exec($curl);
+curl_close($curl);
+print $result;
+
+$curl = curl_init('http://www.techflirt.com');
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+$result = curl_exec($curl);
+$info = curl_getinfo($curl);
+echo '<pre>';
+print_r($info);
+echo '</pre>';
+curl_close($curl);
+
+$curl = curl_init('http://404.techflirt.com');
+$result = curl_exec($curl);
+if ($result == false)
+{
+  echo curl_error($curl);
+}
+curl_close($curl);
 
 ?>
