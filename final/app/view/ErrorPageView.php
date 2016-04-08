a6d250041ee533a4dc050fed5c3b635fdeba7dab
Tom <tgoralczyk1@gmail.com>
2016-04-08 11:04:41 -0400

    Created a new class for showing the errors that might occur anytime something goes wrong with the program

---
+++ final/app/view/ErrorPageView.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\view;
+
+use app\view\html\Button;
+use app\view\html\Paragraph;
+use app\view\html\Link;
+use app\view\html\Heading;
+
+
+class ErrorPageView extends View
+{
+  public function __construct($errors = '')
+  {
+
+  }
+}
+
+?>
