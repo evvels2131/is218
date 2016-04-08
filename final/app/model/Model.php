9026fad6cf041b9206b740897f2a856f583f2ce3
Tom <tgoralczyk1@gmail.com>
2016-04-08 16:40:34 -0400

    Cleaned up the model class

--- final/app/model/Model.php
+++ final/app/model/Model.php
@@ -5,22 +5,9 @@ use app\model\Database;
 
 class Model
 {
-  private $_guid;
-  private $_db;
-
   public function __construct()
   {
-    $this->_db = new Database;
-  }
-
-  public function registerNewUser($username, $email, $password)
-  {
-    $this->_db->register($username, $email, $password);
-  }
-
-  public function login($username, $email, $password)
-  {
-    $this->_db->login($username, $email, $password);
+    // Nothing here for now
   }
 
   // Check if user is logged in
