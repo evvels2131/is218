96580af7951d9b4c7dabfd8da9f0ef3376629e4b
Tom <tgoralczyk1@gmail.com>
2016-04-08 17:40:10 -0400

    Added a function that sanitizes a string

--- final/app/controller/Controller.php
+++ final/app/controller/Controller.php
@@ -3,7 +3,14 @@ namespace app\controller;
 
 abstract class Controller
 {
-
+  // Remove potentially malicious code or tags from user input
+  public function sanitizeString($string)
+  {
+    $string = strip_tags($string);
+    $string = htmlentities($string);
+    $string = stripslashes($string);
+    return $string;
+  }
 }
 
 ?>
