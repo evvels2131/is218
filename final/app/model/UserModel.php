0c52464c4a61343d2560fbafd284c712fca4e4ac
Tom <tgoralczyk1@gmail.com>
2016-04-08 11:44:19 -0400

    Created a new class for the user

---
+++ final/app/model/UserModel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\model;
+
+class UserModel extends Model
+{
+  private $_fname;
+  private $_lname;
+  private $_email;
+  private $_password;
+
+  // Getters and setters
+  public function getFirstName()
+  {
+    return $this->_fname;
+  }
+
+  public function setFirstName($firstName)
+  {
+    $this->_fname = $firstName;
+  }
+}
+
+
+?>
