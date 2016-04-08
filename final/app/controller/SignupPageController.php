b54ee50454f6b2a3f3b9bc175584a8a97b53e51f
Tom <tgoralczyk1@gmail.com>
2016-04-08 10:51:10 -0400

    Handled the post method by redirecting to index.php when user submits data

--- final/app/controller/SignupPageController.php
+++ final/app/controller/SignupPageController.php
@@ -13,7 +13,19 @@ class SignupPageController extends Controller
 
   public function post()
   {
+    $firstname  = $_POST['fname'];
+    $lastname   = $_POST['lname'];
+    $email      = $_POST['email'];
+    $password   = $_POST['pass'];
+    $password2  = $_POST['pass2'];
 
+    // Redirect
+    if (headers_sent()) {
+      echo '<h1><a href="index.php">Home</a></h1>';
+      die('Redirect failed. Please go back to the home page.');
+    } else {
+      exit(header('Location: index.php'));
+    }
   }
 }
 
