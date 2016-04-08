a02c87759e6343bdc55bb28a9e157cc64899d759
Tom <tgoralczyk1@gmail.com>
2016-04-08 17:15:48 -0400

    Added a script that handles going back to the previous page when a button gets clicked

--- final/js/main.js
+++ final/js/main.js
@@ -2,5 +2,9 @@ $(function() {
   console.log('document.onload --> ready!');
   console.log(' ');
 
-  
+  // Go back to the previous page
+  $('#backButton').on('click', function() {
+    window.history.back();
+  });
+
 });
