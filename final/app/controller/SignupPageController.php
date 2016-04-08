3b6e34a486f488f16e34060f8aca619d182e15db
Tom <tgoralczyk1@gmail.com>
2016-04-08 11:51:24 -0400

    Checked to see if  array is set and not empty before proceeding, otherwise, display a page that notifies the user about any problems that have occured

--- final/app/controller/SignupPageController.php
+++ final/app/controller/SignupPageController.php
@@ -2,6 +2,8 @@
 namespace app\controller;
 
 use app\view\SignupPageView;
+use app\model\UserModel;
+use app\view\ErrorPageView;
 
 class SignupPageController extends Controller
 {
@@ -13,19 +15,27 @@ class SignupPageController extends Controller
 
   public function post()
   {
-    $firstname  = $_POST['fname'];
-    $lastname   = $_POST['lname'];
-    $email      = $_POST['email'];
-    $password   = $_POST['pass'];
-    $password2  = $_POST['pass2'];
+    if (isset($_POST) && !empty($_POST))
+    {
+      $firstname  = $_POST['fname'];
+      $lastname   = $_POST['lname'];
+      $email      = $_POST['email'];
+      $password   = $_POST['pass'];
+      $password2  = $_POST['pass2'];
+    }
+    else
+    {
+      $error_info = 'Something went wrong!';
+      $errorPageView = new ErrorPageView($error_info);
+    }
 
     // Redirect
-    if (headers_sent()) {
+    /*if (headers_sent()) {
       echo '<h1><a href="index.php">Home</a></h1>';
       die('Redirect failed. Please go back to the home page.');
     } else {
       exit(header('Location: index.php'));
-    }
+    }*/
   }
 }
 
