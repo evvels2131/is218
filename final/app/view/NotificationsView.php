adf10c212a95444b0861deab6aa06075254d7c23
Tom <tgoralczyk1@gmail.com>
2016-04-08 17:16:46 -0400

    Added a new page that displays information after logging in or creating a new user

---
+++ final/app/view/NotificationsView.php
@@ -0,0 +1,26 @@
+<?php
+namespace app\view;
+
+use app\view\html\Button;
+use app\view\html\Paragraph;
+use app\view\html\Link;
+use app\view\html\Heading;
+
+class NotificationsView extends View
+{
+  public function __construct($notifications = '')
+  {
+    echo parent::htmlHeader('Notifications');
+
+    $heading = parent::htmlAlertDiv('info', Heading::newHeading('h4', '<b>Results</b>'));
+
+    echo parent::htmlDiv($heading, 8);
+
+    echo parent::htmlDiv($notifications, 4);
+
+    echo parent::htmlDiv('<button type="button" id="backButton" class="btn btn-primary">Home</button>', 4);
+
+    echo parent::htmlFooter();
+  }
+}
+?>
