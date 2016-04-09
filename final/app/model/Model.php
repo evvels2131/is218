0bac1014060dd8ea8ebc9ecf25875d3a16cd77d0
Tom <tgoralczyk1@gmail.com>
2016-04-09 17:44:40 -0400

    Deleted unnecessary code

--- final/app/model/Model.php
+++ final/app/model/Model.php
@@ -5,30 +5,6 @@ use app\model\Database;
 
 class Model
 {
-  public function __construct()
-  {
-    // Nothing here for now
-  }
-
-  // Check if user is logged in
-  public function is_loggedin()
-  {
-    if (isset($_SESSION['user_session']))
-    {
-      return true;
-    }
-    else
-    {
-      return false;
-    }
-  }
-
-  // Log out
-  public function logout()
-  {
-    session_unset();
-    return true;
-  }
 
 }
 ?>
