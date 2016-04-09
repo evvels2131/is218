eb059612d61c1d544132f7c0ee526a0e3ab4d9e8
Tom <tgoralczyk1@gmail.com>
2016-04-09 18:35:48 -0400

    The profile page view now displays login attempts

--- final/app/view/ProfilePageView.php
+++ final/app/view/ProfilePageView.php
@@ -5,10 +5,11 @@ use app\view\html\Button;
 use app\view\html\Paragraph;
 use app\view\html\Link;
 use app\view\html\Heading;
+use app\view\html\ListHTML;
 
 class ProfilePageView extends View
 {
-  public function __construct($data = '')
+  public function __construct($loginAttempts = '')
   {
     echo parent::htmlHeader('Profile Information');
 
@@ -16,6 +17,10 @@ class ProfilePageView extends View
     $heading = Heading::newHeading('h4', $text);
     $content = parent::htmlAlertDiv('info', $heading);
     echo parent::htmlDiv($content, 8);
+
+    $content = Heading::newHeading('h4', 'You have logged in on the following dates:');
+    $content .= ListHTML::loginAttemptsList($loginAttempts);
+    echo parent::htmlDiv($content, 4);
 
     echo parent::htmlFooter();
   }
