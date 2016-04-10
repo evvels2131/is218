4409599e8887899435bd633b53f52d0be622552b
Tom <tgoralczyk1@gmail.com>
2016-04-10 17:34:03 -0400

    Created a login page view class

---
+++ final/app/view/LoginPageView.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\view;
+
+use app\view\html\Form;
+use app\view\html\InputField;
+use app\view\html\Button;
+use app\view\html\Paragraph;
+use app\view\html\Link;
+use app\view\html\Heading;
+
+class LoginPageView extends View
+{
+  
+}
+
+?>
