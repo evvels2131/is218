2d4ea1800d0e45b6727f57f945f01e99203666e0
Tom <tgoralczyk1@gmail.com>
2016-04-10 17:27:11 -0400

    Created a new class for the login page

---
+++ final/app/controller/LoginPageController.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\controller;
+
+use app\view\LoginPageView;
+use app\model\UserModel;
+use app\view\NotificationsView;
+
+class LoginPageController extends Controller
+{
+  
+}
+
+
+?>
