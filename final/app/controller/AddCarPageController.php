7295c9e7e61cf9b5b68e692e085223b7beab6a5d
Tom <tgoralczyk1@gmail.com>
2016-04-09 20:03:41 -0400

    Started filling out the post method to handle the users post requests

--- final/app/controller/AddCarPageController.php
+++ final/app/controller/AddCarPageController.php
@@ -2,6 +2,7 @@
 namespace app\controller;
 
 use app\view\AddCarView;
+use app\view\NotificationsView;
 use app\model\CarModel;
 use app\model\UserModel;
 
@@ -14,7 +15,22 @@ class AddCarPageController extends Controller
 
   public function post()
   {
+    if (isset($_POST) && !empty($_POST))
+    {
+      // vin, price, condition
+      $vin        = parent::sanitizeString($_POST['vin']);
+      $price      = parent::sanitizeString($_POST['price']);
+      $condition  = parent::sanitizeString($_POST['condition']);
 
+      echo '<pre>';
+      print_r($_POST);
+      echo '</pre>';
+    }
+    else
+    {
+      $result = 'Oops! Something went wrong. <br />Please try again.';
+      $notificationsView = new NotificationsView($result);
+    }
   }
 }
 
