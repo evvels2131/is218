dfdeb57c25453f4bc5e0d7d842e335abd56d637b
Tom <tgoralczyk1@gmail.com>
2016-04-09 20:20:36 -0400

    Added a getter and setter for the id property

--- final/app/model/CarModel.php
+++ final/app/model/CarModel.php
@@ -10,6 +10,17 @@ class CarModel extends Model
   private $_condition;
   private $_price;
   private $_img_url;
+
+  // Getters and setters
+  public function getId()
+  {
+    return $this->_id;
+  }
+
+  public function setId($id)
+  {
+    $this->_id = $id;
+  }
 }
 
 ?>
