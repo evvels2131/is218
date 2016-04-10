c0829a06efc5012684f060f4a78f031072b39cbd
Tom <tgoralczyk1@gmail.com>
2016-04-10 17:32:20 -0400

    Finished up the post method within the login controller

--- final/app/controller/LoginPageController.php
+++ final/app/controller/LoginPageController.php
@@ -16,8 +16,28 @@ class LoginPageController extends Controller
   {
     if (isset($_POST['email']) && isset($_POST['password']))
     {
-      
+      $email    = parent::sanitizeString($_POST['email']);
+      $password = parent::sanitizeString($_POST['password']);
+
+      $user = new UserModel();
+      $user->setEmail($email);
+      $user->setPassword($password);
+
+      if ($user->login())
+      {
+        $message = 'Congratulations! You have successfully logged in.';
+      }
+      else
+      {
+        $message = 'Oops! Incorrect password and email. <br />Please go back and try again.';
+      }
     }
+    else
+    {
+      $message = 'Oops! Something went wrong. <br />Please go back and try again.';
+    }
+
+    $notificationsView = new NotificationsView($message);
   }
 }
 
