9c328aff755a7ceb2051b73e66745cb53dd958d5
Tom <tgoralczyk1@gmail.com>
2016-04-10 17:28:30 -0400

    Created two functions for get and post within the login page controller

--- final/app/controller/LoginPageController.php
+++ final/app/controller/LoginPageController.php
@@ -7,7 +7,18 @@ use app\view\NotificationsView;
 
 class LoginPageController extends Controller
 {
-  
+  public function get()
+  {
+    $loginPageView = new LoginPageView();
+  }
+
+  public function post()
+  {
+    if (isset($_POST['email']) && isset($_POST['password']))
+    {
+      
+    }
+  }
 }
 
 
