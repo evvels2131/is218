09939755167213c8a1bd12f378eb7c8e7c6b99d6
Tom <tgoralczyk1@gmail.com>
2016-04-09 20:19:29 -0400

    Created a new class for the car model

---
+++ final/app/model/CarModel.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\model;
+
+use app\model\Database;
+
+class CarModel extends Model
+{
+  private $_id;
+  private $_vin;
+  private $_condition;
+  private $_price;
+  private $_img_url;
+}
+
+?>
