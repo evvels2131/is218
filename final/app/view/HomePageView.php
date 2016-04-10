918d9ed00f084441739ddda1e7f87ccab4d0d786
Tom <tgoralczyk1@gmail.com>
2016-04-10 18:04:13 -0400

    Changed the home page so that it displays different messages based whether there are any cars for sale or not

--- final/app/view/HomePageView.php
+++ final/app/view/HomePageView.php
@@ -10,40 +10,23 @@ use app\view\html\Heading;
 
 class HomePageView extends View
 {
-  public function __construct($data = '')
+  public function __construct($cars = '')
   {
     echo parent::htmlHeader('Home');
 
-    $content = '';
-    if (isset($_SESSION['user_session']))
+    if (!empty($cars))
     {
-      $message = 'Welcome <b>' . $_SESSION['user_fname'] . ' ' . $_SESSION['user_lname'] . '</b>!';
-      $heading = parent::htmlAlertDiv('info', Heading::newHeading('h4', $message));
-      echo parent::htmlDiv($heading, 8);
+      $heading = Heading::newHeading('h4', 'Cars for sale');
+      $content = parent::htmlAlertDiv('info', $heading);
+      echo parent::htmlDiv($content, 8);
     }
     else
     {
-      $heading = parent::htmlAlertDiv('info', Heading::newHeading('h4', '<b>Welcome!</b>
-        Sing in or sing up below'));
-      echo parent::htmlDiv($heading, 8);
-
-      $username = InputField::newInputField('text', 'email', 'Email');
-      $password = InputField::newInputField('password', 'password', 'Password');
-      $submit   = Button::newButton('submit', 'btn-primary', 'Sing in');
-
-      $form = new Form('index.php', 'POST');
-      $form->addNewInput($username);
-      $form->addNewInput($password);
-      $form->addNewInput($submit);
-
-      $content .= $form->getForm();
-
-      $content .= Paragraph::newParagraph('Not a member yet? Please sign up below!');
-      $content .= Link::newLink('Sing up', 'index.php?page=signup', '_self');
+      $heading = Heading::newHeading('h4', 'There are currently no cars for sale');
+      $content = parent::htmlAlertDiv('danger', $heading);
+      echo parent::htmlDiv($content, 8);
     }
 
-    // Generate HTML and display content
-    echo parent::htmlDiv($content, 4);
     echo parent::htmlFooter();
   }
 }
