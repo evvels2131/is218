1b20c67cea851562c37eef25ae33c13d98af40f9
Tom <tgoralczyk1@gmail.com>
2016-04-11 08:43:56 -0400

    Added some content to the car details page

--- final/app/view/CarDetailsView.php
+++ final/app/view/CarDetailsView.php
@@ -8,7 +8,13 @@ use app\view\html\ListHTML;
 
 class CarDetailsView extends View
 {
-  
+  echo parent::htmlHeader('Car Details');
+
+  $heading = Heading::newHeading('h4', 'Car details');
+  $content = parent::htmlAlertDiv('info', $heading);
+  echo parent::htmlDiv($content, 8);
+
+  echo parent::htmlFooter();
 }
 
 ?>
