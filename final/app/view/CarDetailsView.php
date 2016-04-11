ebfb23c86a0706f016025b37703ebd70d79a5fe8
Tom <tgoralczyk1@gmail.com>
2016-04-11 08:42:39 -0400

    Created a new view for the car details page

---
+++ final/app/view/CarDetailsView.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\view;
+
+use app\view\html\Paragraph;
+use app\view\html\Link;
+use app\view\html\Heading;
+use app\view\html\ListHTML;
+
+class CarDetailsView extends View
+{
+  
+}
+
+?>
