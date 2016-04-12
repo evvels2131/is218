fb50778e61bbeb134512fb46ba35af8c3f7cf099
Tom <tgoralczyk1@gmail.com>
2016-04-12 13:53:58 -0400

    Created a user collection with a function that creates a new user model and returns it

---
+++ final/app/collection/UserCollection.php
@@ -0,0 +1,17 @@
+<?php
+namespace app\collection;
+
+use app\model\UserModel;
+
+class UserCollection extends Collection
+{
+  // Create a new user model
+  public function create()
+  {
+    $user = new UserModel;
+    return $user;
+  }
+}
+
+
+?>
