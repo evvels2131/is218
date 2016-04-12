f00327e4e5789678064b5c900af39ae765749b8d
Tom <tgoralczyk1@gmail.com>
2016-04-12 14:28:19 -0400

    The construct now will create cars table if it doesn't exist in the database

--- final/app/model/CarModel.php
+++ final/app/model/CarModel.php
@@ -1,7 +1,8 @@
 <?php
 namespace app\model;
 
-use app\model\Database;
+use app\model\DatabaseConnection;
+use \PDO;
 
 class CarModel extends Model
 {
@@ -21,6 +22,36 @@ class CarModel extends Model
       $this->_id = $id;
     } else {
       $this->_id = uniqid('car_', false);
+    }
+
+    // Create a table for cars if not existent in the database
+    try
+    {
+      $dbconn = DatabaseConnection::getConnection();
+
+      $stmt = $dbconn->prepare('CREATE TABLE IF NOT EXISTS cars (
+        car_id CHAR(17) NOT NULL,
+        vin CHAR(17) NOT NULL,
+        make VARCHAR(15) DEFAULT NULL,
+        model VARCHAR(15) DEFAULT NULL,
+        year VARCHAR(4) DEFAULT NULL,
+        price VARCHAR(10) DEFAULT NULL,
+        cond VARCHAR(10) DEFAULT NULL,
+        img_url VARCHAR(20) DEFAULT NULL,
+        added_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        created_by CHAR(18) NOT NULL,
+        PRIMARY KEY(car_id),
+        FOREIGN KEY fk_user(created_by)
+        REFERENCES users(user_id)
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+      )ENGINE=InnoDB');
+      $stmt->execute();
+    }
+    catch (\PDOException $e)
+    {
+      echo 'Database error: ' . $e->getMessage();
+      die();
     }
   }
 
