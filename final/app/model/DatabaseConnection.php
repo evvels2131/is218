b0898a973e3c607aa4a5c7a76b448f3af887b26a
Tom <tgoralczyk1@gmail.com>
2016-04-12 14:11:41 -0400

    Created a new class for the database connection

---
+++ final/app/model/DatabaseConnection.php
@@ -0,0 +1,39 @@
+<?php
+namespace app\model;
+
+class DatabaseConnection extends Database
+{
+  protected static $_db;
+
+  const DB_USER     = 'root';
+  const DB_PASSWORD = 'root';
+  const DB_HOST     = 'localhost';
+  const DB_NAME     = 'is218';
+
+  private function __construct()
+  {
+    try
+    {
+      self::$_db = new PDO('mysql:host=' . self::DB_HOST . ';dbname=' . self::DB_NAME,
+        array(PDO::ATTR_PERSISTENT => true));
+      self::$_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (\PDOException $e)
+    {
+      echo 'Database error: ' . $e->getMessage();
+      die();
+    }
+  }
+
+  public static function getConnection()
+  {
+    if (!self::$_db)
+    {
+      new DatabaseConnection();
+    }
+
+    return self::$_db;
+  }
+}
+
+?>
