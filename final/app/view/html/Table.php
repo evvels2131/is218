fc8c7653fa5937fb0e1a4ddb0ba6472b7f586251
Tom <tgoralczyk1@gmail.com>
2016-04-13 09:34:09 -0400

    Made a few changes to the function that generates the car table

--- final/app/view/html/Table.php
+++ final/app/view/html/Table.php
@@ -3,43 +3,42 @@ namespace app\view\html;
 
 class Table extends HTML
 {
-  // Creates a table with cars for sale
-  public static function createCarsTable($cars)
+  // Create a table with cars for homepage
+  public static function displayCarsTable($cars)
   {
     $tableHTML = '<table class="table table-striped">';
     $tableHTML .= '<thead><tr>';
+
     foreach ($cars[0] as $key => $val)
     {
-      if ($key == 'user_id') {
+      if ($key == 'UserID') {
         continue;
+      } else if ($key == 'CarID') {
+        $tableHTML .= '<th>Details</th>';
       } else {
         $tableHTML .= '<th>' . $key . '</th>';
       }
     }
     $tableHTML .= '</tr></thead>';
     $tableHTML .= '<tbody>';
+
     foreach ($cars as $key => $value)
     {
       $tableHTML .= '<tr>';
       foreach ($value as $property => $val)
       {
-        if ($property == 'user_id') {
+        if ($property == 'UserID') {
           $user_id = $val;
-          continue;
-        } else if ($property == 'Salesman') {
-          $tableHTML .= '<td><a href="index.php?page=profile&id=' . $user_id. '">' . $val . '</a></td>';
-        } else if ($property == 'Vin') {
-          $tableHTML .= '<td><a href="index.php?page=cardetails&id=' . $val . '">' . $val . '</a>';
-        }
-        else {
+        } else if ($property == 'Salesperson') {
+          $tableHTML .= '<td><a href="index.php?page=profile&id=' . $user_id . '">' . $val . '</a></td>';
+        } else if ($property == 'CarID') {
+          $tableHTML .= '<td><a href="index.php?page=cardetails&id=' . $val . '">Click Here</a></td>';
+        } else {
           $tableHTML .= '<td>' . $val . '</td>';
         }
-
       }
-      $tableHTML .= '</tr>';
     }
     $tableHTML .= '</tbody>';
-
     $tableHTML .= '</table>';
 
     return $tableHTML;
