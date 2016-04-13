36f00b7e886a6fcfea0c19d276cf5f7b83c22e87
Tom <tgoralczyk1@gmail.com>
2016-04-13 10:16:31 -0400

    Implemented a function that grabs detailes about a car from an API

--- final/app/controller/Controller.php
+++ final/app/controller/Controller.php
@@ -11,6 +11,33 @@ abstract class Controller
     $string = stripslashes($string);
     return $string;
   }
+
+  // Get detailed information about a specific car
+  public function getCarsDetails($vin)
+  {
+    $api_key = 'u48d7aetdk4yz6pwqnzd62ez';
+    $url = 'https://api.edmunds.com/api/vehicle/v2/vins/' . $vin .
+      '?fmt=json&api_key=' . $api_key;
+
+    try
+    {
+      $ch = curl_init();
+
+      curl_setopt($ch, CURLOPT_URL, $url);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+      $output = curl_exec($ch);
+      curl_close($ch);
+
+      $response = json_decode($output);
+
+      return $response;
+    }
+    catch (Exception $e)
+    {
+      echo 'API Error: ' . $e->getMessage();
+    }
+  }
 }
 
 ?>
