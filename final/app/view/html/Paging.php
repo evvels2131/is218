4f8306509cdc3cf89b2e92f65f052640429a4fe3
Tom <tgoralczyk1@gmail.com>
2016-04-14 12:52:54 -0400

    Created a new class that creates paging links based on the amount of records in the cars table in the database

---
+++ final/app/view/html/Paging.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\view\html;
+
+abstract class Paging
+{
+  public static function getPagingLinks($amount)
+  {
+    $pagingHTML = '<ul class="pagination">';
+    for ($i = 1; $i < $amount + 1; $i++)
+    {
+      $pagingHTML .= '<li><a href="index.php?page_no=' . $i . '">' . $i . '</a></li>';
+    }
+    $pagingHTML .= '</ul>';
+
+    return $pagingHTML;
+  }
+}
+?>
