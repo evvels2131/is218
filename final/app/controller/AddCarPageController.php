b659e60f634473c2e597fe7893d7b58cb775b60f
Tom <tgoralczyk1@gmail.com>
2016-04-14 08:20:51 -0400

    Added a function that saves the pictures that are being uploaded when adding a new car

--- final/app/controller/AddCarPageController.php
+++ final/app/controller/AddCarPageController.php
@@ -40,6 +40,7 @@ class AddCarPageController extends Controller
       {
         $result = 'Congratulations! You\'ve successfully added a new car.';
         $type = 'success';
+        $result = self::saveFile();
       }
     }
     else
@@ -49,6 +50,69 @@ class AddCarPageController extends Controller
     }
 
     $notificationsView = new NotificationsView($result, $type);
+  }
+
+  public function saveFile()
+  {
+    $name     = $_FILES['file']['name'];
+    $tmpName  = $_FILES['file']['tmp_name'];
+    $error    = $_FILES['file']['error'];
+    $size     = $_FILES['file']['size'];
+    $ext      = strtolower(pathinfo($name, PATHINFO_EXTENSION));
+
+    $switch ($error)
+    {
+      case UPLOAD_ERR_OK;
+        $valid = true;
+        // Validate file extensions
+        if (!in_array($ext, array('jpg', 'jpeg', 'png', 'gif')))
+        {
+          $valid = false;
+          $response = 'Invalid file extension';
+        }
+        // Validate file size
+        if ($size / 1024 / 2014 > 3)
+        {
+          $valid = false;
+          $response = 'File size is exceeding maximum allowed size';
+        }
+        // Upload file
+        if ($valid)
+        {
+          $targetPath = dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR .
+            'uploads' . DIRECTORY_SEPARATOR . $name;
+          move_uploaded_file($tmpName, $targetPath);
+        }
+        break;
+      case UPLOAD_ERR_INI_SIZE:
+        $response = 'The uploaded file exceeds the upload_max_filesize directive
+          in php.ini';
+        break;
+      case UPLOAD_ERR_FORM_SIZE:
+        $response = 'The uploaded file exceeds the MAX_FILE_SIZE directive that was
+          specified in the HTML form.';
+        break;
+      case UPLOAD_ERR_PARTIAL:
+        $response = 'The file was only partially uploaded';
+        break;
+      case UPLOAD_ERR_NO_FILE;
+        $response = 'No file was uploaded';
+        break;
+      case UPLOAD_ERR_NO_TMP_DIR:
+        $response = 'Missing a temporary folder.';
+        break;
+      case UPLOAD_ERR_CANT_WRITE:
+        $response = 'Failed to write to disk';
+        break;
+      case UPLOAD_ERR_EXTENSION:
+        $response = 'File uploaded stopped by extension';
+        break;
+      default:
+        $response = 'Unknown error';
+        break;
+    }
+
+    return $response;
   }
 }
 
