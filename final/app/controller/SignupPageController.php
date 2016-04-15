16bf83c3e1a259da60571e408ced19492f732a5a
Tom <tgoralczyk1@gmail.com>
2016-04-14 21:42:04 -0400

    Improved the sign up process by checking for various requirements before processing the data and saving it into the database

--- final/app/controller/SignupPageController.php
+++ final/app/controller/SignupPageController.php
@@ -14,50 +14,84 @@ class SignupPageController extends Controller
 
   public function post()
   {
-    if (isset($_POST) && !empty($_POST))
+    if ($_POST['form'])
     {
-      $firstname  = parent::sanitizeString($_POST['fname']);
-      $lastname   = parent::sanitizeString($_POST['lname']);
-      $email      = parent::sanitizeString($_POST['email']);
-      $password   = parent::sanitizeString($_POST['pass']);
-      $password2  = parent::sanitizeString($_POST['pass2']);
+      $allowed = array();
+      $allowed[] = 'form';
+      $allowed[] = 'fname';
+      $allowed[] = 'lname';
+      $allowed[] = 'email';
+      $allowed[] = 'pass';
+      $allowed[] = 'pass2';
 
-      $hashedPassword = parent::hashPassword($password);
+      $sent = array_keys($_POST);
 
-      $userCollection = new UserCollection();
-      $user = $userCollection->create();
-
-      $user->setFirstName($firstname);
-      $user->setLastName($lastname);
-      $user->setEmail($email);
-      $user->setPassword($hashedPassword);
-
-      if ($user->register() == true)
+      if ($allowed == $sent)
       {
-        $result = 'Cogratulations! You\'ve successfully registered.';
-        $type = 'success';
+        if (isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['email'])
+          && isset($_POST['pass']) && isset($_POST['pass2']))
+        {
+          // Check if the token from form matches the one saved in the session
+          if (isset($_SESSION['token']) && $_POST['form'] != $_SESSION['token']) {
+            $message = 'Something went wrong. Please try again.';
+            $type = 'danger';
+            $notification = new NotificationsView($message, $type);
+            exit();
+          }
+
+          // Check if the email is valid
+          if (!parent::isValidEmail($_POST['email'])) {
+            $message = 'Incorrect email. Please provide a valid email';
+            $type = 'danger';
+            $notification = new NotificationsView($message, $type);
+            exit();
+          }
+
+          // Check if passwords are matching
+          if ($_POST['pass'] != $_POST['pass2']) {
+            $message = 'Passwords are not matching. Please go back and try again.';
+            $type = 'danger';
+            $notification = new NotificationsView($message, $type);
+            exit();
+          }
+
+          // User data
+          $clean_fname  = parent::sanitizeString($_POST['fname']);
+          $clean_lname  = parent::sanitizeString($_POST['lname']);
+          $clean_email  = parent::sanitizeString($_POST['email']);
+          $clean_pass   = parent::sanitizeString($_POST['pass']);
+          $pass_hash    = parent::hashPassword($clean_pass);
+
+          $userCollection = new UserCollection();
+
+          $user = $userCollection->create();
+          $user->setFirstName($clean_fname);
+          $user->setLastName($clean_lname);
+          $user->setEmail($clean_email);
+          $user->setPassword($pass_hash);
+
+          if ($user->register()) {
+            $message = 'Congratulations! You\'ve successfully registered.';
+            $type = 'success';
+          } else {
+            $message = 'Something went wrong! Please try again.';
+            $type = 'danger';
+          }
+        }
+        else
+        {
+          $message = 'Make sure you\'ve provided all information.
+            Please go back and try again.';
+          $type = 'danger';
+        }
       }
       else
       {
-        $result = 'Oops! Something went wrong.<br />' . 'Please try again.';
+        $message = 'Something went wrong. Please go back and try again.';
         $type = 'danger';
       }
     }
-    else
-    {
-      $result = 'Oops! Something went wrong.<br />' . 'Please try again.';
-      $type = 'danger';
-    }
-
-    $notificationsView = new NotificationsView($result, $type);
-
-    // Redirect
-    /*if (headers_sent()) {
-      echo '<h1><a href="index.php">Home</a></h1>';
-      die('Redirect failed. Please go back to the home page.');
-    } else {
-      exit(header('Location: index.php'));
-    }*/
+    $notification = new NotificationsView($message, $type);
   }
 }
 
