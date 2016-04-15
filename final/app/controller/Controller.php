8f62044156ce17dbafd1aec3f384398975dacf68
Tom <tgoralczyk1@gmail.com>
2016-04-14 21:09:13 -0400

    Added a function that hashes a password before storing it into the database

--- final/app/controller/Controller.php
+++ final/app/controller/Controller.php
@@ -50,6 +50,17 @@ abstract class Controller
       echo 'API Error: ' . $e->getMessage();
     }
   }
+
+  // Password hashing
+  public function hashPassword($password)
+  {
+    $options = [
+      'cost' => 11,
+      'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM)
+    ];
+
+    return password_hash($password, PASSWORD_BCRYPT, $options);
+  }
 }
 
 ?>
