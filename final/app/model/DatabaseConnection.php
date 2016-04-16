8d07fb94817ab90722bfc13d4ff2318058c70ec3
Tom <tgoralczyk1@gmail.com>
2016-04-15 22:16:55 -0400

    Minor changes

--- final/app/model/DatabaseConnection.php
+++ final/app/model/DatabaseConnection.php
@@ -7,10 +7,15 @@ class DatabaseConnection extends Database
 {
   protected static $_db;
 
-  const DB_USER     = 'tg77';
-  const DB_PASSWORD = 'jxosRjx3L';
-  const DB_HOST     = 'sql2.njit.edu';
-  const DB_NAME     = 'tg77';
+  //const DB_USER     = 'tg77';
+  //const DB_PASSWORD = 'jxosRjx3L';
+  //const DB_HOST     = 'sql2.njit.edu';
+  //const DB_NAME     = 'tg77';
+
+  const DB_USER     = 'root';
+  const DB_PASSWORD = 'root';
+  const DB_HOST     = 'localhost';
+  const DB_NAME     = 'is2188';
 
   private function __construct()
   {
