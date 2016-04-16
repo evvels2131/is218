600f6698a6554689412dad5c24b2fadc97ca9a29
Tom <tgoralczyk1@gmail.com>
2016-04-15 20:17:48 -0400

    Created a view for the confirmation feature

---
+++ final/app/view/ConfirmationView.php
@@ -0,0 +1,22 @@
+<?php
+namespace app\view;
+
+use app\view\html\Paragraph;
+use app\view\html\Link;
+
+class ConfirmationView extends View
+{
+  public function __construct()
+  {
+    echo parent::htmlHeader('Confirmation Page');
+
+    $heading = Heading::newHeading('h4', 'Confirmation page');
+    $content = parent::htmlAlertDiv('info', $heading);
+    echo parent::htmlDiv($content, 8);
+
+
+    echo parent::htmlFooter();
+  }
+}
+
+?>
