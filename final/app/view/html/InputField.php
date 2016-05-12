46382415b9801ae3f36dd1af09f490fd270cacf8
Tom <tgoralczyk1@gmail.com>
2016-05-12 18:47:40 -0400

    Added a new function that creates a hidden input field for the image url of a specific car

--- final/app/view/html/InputField.php
+++ final/app/view/html/InputField.php
@@ -45,12 +45,17 @@ class InputField extends HTML
   {
     if ($disabled) {
       $input = '<input type="' . $type . '" name="' . $name . '"
-        class="form-control" disabled value="' . $value . '" placeholder="' . $placeholder . '" >';
+        class="form-control disabled" value="' . $value . '" placeholder="' . $placeholder . '" >';
     } else {
       $input = '<input type="' . $type . '" name="' . $name . '"
         class="form-control" value="' . $value . '" placeholder="' . $placeholder . '" >';
     }
     return $input;
+  }
+
+  public static function hiddenImageField($img_url) 
+  {
+    return '<input type="text" name="img_url" value="' . $img_url . '" class="hp" />';
   }
 }
 
